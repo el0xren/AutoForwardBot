01dbbbfd8020620b3c1a4f9ee6b2473b61d55540
KassemSYR <kassemkanaan23@gmail.com>
2022-01-28 15:06:56 +0200

    gix

--- tg_bot/uplugins/control.py
+++ tg_bot/uplugins/control.py
@@ -5,6 +5,7 @@ from tg_bot.tg_bot import TG_UBOT as ubot
 
 async def send_msg(client, message, text):
     if message.from_user.id == OWNER_ID:
+        message.delete()
         await message.reply(text)
     else:
         await message.edit_text(text)
@@ -18,19 +19,20 @@ async def alive_msg(client, message):
     await send_msg(
         client,
         message,
-        "<b>Hey ya, I'm Alive 🙌</b>\n send <code>/help</code> to see avaliable commands",
-    )
+        "<b>Hey ya, I'm Alive 🙌</b>\n send <code>/help</code> to see avaliable commands" )
 
 
 @ubot.on_message(
-    (filters.me | filters.user(OWNER_ID)) & filters.command("help", ["/", "."])
+    (filters.me | filters.user(OWNER_ID))
+    & filters.command("help", ["/", "."])
 )
 async def help_msg(client, message):
     await send_msg(
         client,
         message,
         "\n\n🤖 <b>Avaliable Commands:</b>\n- /alive or /start: to check if bot is alive\n- /join or /add: followed by channel link or username\n- /leave or /remove: followed by channel link or username\n\n <b>You can Prefix commands with / or .</b>",
-    )
+ )
+
 
 
 @ubot.on_message(
@@ -38,12 +40,8 @@ async def help_msg(client, message):
 )
 async def join_channel(client, message):
     if len(message.command) != 2:
-        await send_msg(
-            client,
-            message,
-            "⚠ Please use <b>/join or /add:</b> followed by channel <u>link</u> or <u>username</u>\ni.e: <code>/add @telegram</code>",
-        )
-        return
+    	await send_msg(client, message, "⚠ Please use <b>/join or /add:</b> followed by channel <u>link</u> or <u>username</u>\ni.e: <code>/add @telegram</code>")
+    	return
     try:
         await send_msg(
             client,
@@ -65,12 +63,8 @@ async def join_channel(client, message):
 )
 async def leave_channel(client, message):
     if len(message.command) != 2:
-        await send_msg(
-            client,
-            message,
-            "⚠ Please use <b>/leave or /remove:</b> followed by channel <u>link</u> or <u>username</u>\ni.e: <code>/remove @telegram</code>",
-        )
-        return
+    	await send_msg(client, message, "⚠ Please use <b>/leave or /remove:</b> followed by <u>username</u>\ni.e: <code>/remove @telegram</code>")
+    	return
     try:
         await send_msg(
             client,
